abb118e22cda870cb633ce441f7596abc5b9b606
xuyangbai <653823597@qq.com>
2019-04-10 13:46:30 +0800

    update a fast version of ppf calculation

--- input_preparation.py
+++ input_preparation.py
@@ -62,42 +62,57 @@ def build_local_patch(ref_pcd, pcd, neighbor):
     for j, ref_point, ref_point_normal, inds in zip(range(num_ref_point), ref_pcd.points, ref_pcd.normals, neighbor):
         ppfs = np.zeros([num_point_per_patch, 4])
         # for each point in this local patch
-        for i, ind in zip(range(num_point_per_patch), inds):
-            ppf = _ppf(ref_point, ref_point_normal, pcd.points[ind], pcd.normals[ind])
-            ppfs[i] = ppf
+        ppfs = _ppf(ref_point, ref_point_normal, np.asarray(pcd.points)[inds], np.asarray(pcd.normals)[inds])
+        # origin version: calculate one ppf each time, very SLOW!
+        # for i, ind in zip(range(num_point_per_patch), inds):
+        #     ppf = _ppf(ref_point, ref_point_normal, pcd.points[ind], pcd.normals[ind])
+        #     ppfs[i] = ppf
         local_patch[j] = ppfs
     return local_patch
 
 
 def _ppf(point1, normal1, point2, normal2):
-    d = point1 - point2
-    len_d = np.sqrt(np.dot(d, d))
-    # len_normal1 = np.sqrt(np.dot(normal1, normal1))
-    # len_normal2 = np.sqrt(np.dot(normal2, normal2))
-    dim1 = np.dot(normal1, d) / len_d
-    dim2 = np.dot(normal2, d) / len_d
-    dim3 = np.dot(normal1, normal2)
-    return np.array([dim1, dim2, dim3, len_d])
+    # origin version: calculate one ppf each time, very SLOW!
+    # d = point1 - point2
+    # len_d = np.sqrt(np.dot(d, d))
+    # dim1 = np.dot(normal1, d) / len_d
+    # dim2 = np.dot(normal2, d) / len_d
+    # dim3 = np.dot(normal1, normal2)
+    # return np.array([dim1, dim2, dim3, len_d])
+
+    d = point1 - point2  # [1024, 3]
+    len_d = np.sqrt(np.diag(np.dot(d, d.transpose())))  # [1024, 1]
+    # element wise multiply https://docs.scipy.org/doc/numpy/reference/generated/numpy.multiply.html
+    dim1 = np.sum(np.multiply(normal1, d), axis=1) / len_d  # [1024, 1]
+    dim2 = np.sum(np.multiply(normal2, d), axis=1) / len_d  # [1024, 1]
+    dim3 = np.sum(np.multiply(normal1, normal2), axis=1)
+    return np.array([dim1, dim2, dim3, len_d]).transpose()
 
 
 def input_preprocess(data_dir, id, save_dir):
     # rgbd -> point cloud
+    # start_time = time.time()
     pcd = rgbd_to_point_cloud(data_dir, id)
+    # print("rgbd->pcd: ", time.time() - start_time)
 
     # calculate local normal for point cloud
     cal_local_normal(pcd)
+    # print("cal normal: ", time.time() - start_time)
 
     # select referenced points (default number 2048)
     ref_pcd = select_referenced_point(pcd)
-    # ref_pts = np.asarray(ref_pcd.points)
+    # print("select ref: ", time.time() - start_time)
+    ref_pts = np.asarray(ref_pcd.points)
     # assert ref_pts.shape[0] == 2048
 
     # collect local patch for each reference point
     neighbor = collect_local_neighbor(ref_pcd, pcd)
-    # assert len(patches) == ref_pts.shape[0]
+    # print("collect neighbor:", time.time() - start_time)
+    # assert len(neighbor) == ref_pts.shape[0]
 
     # calculate the point pair feature for each patch
     local_patch = build_local_patch(ref_pcd, pcd, neighbor)
+    # print("cal ppf for each pair:", time.time() - start_time)
 
     # save the local_patch and reference point cloud for one point cloud fragment.
     if not os.path.exists(save_dir):
