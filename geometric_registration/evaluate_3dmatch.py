18003b14856273dbe1adb4d696e6e1ae95bbf5fe
xuyangbai <653823597@qq.com>
2019-04-18 14:50:35 +0800

    update evaluate_prepration

--- geometric_registration/evaluate_3dmatch.py
+++ geometric_registration/evaluate_3dmatch.py
@@ -27,13 +27,11 @@ def calculate_M(source_desc, target_desc):
     # print(time.time() - start_time)
     # return np.array(result)
 
-    start_time = time.time()
     kdtree_s = KDTree(target_desc)
     sourceNNdis, sourceNNidx = kdtree_s.query(source_desc, 1)
     kdtree_t = KDTree(source_desc)
     targetNNdis, targetNNidx = kdtree_t.query(target_desc, 1)
     result = []
-    # print(time.time() - start_time)
     for i in range(len(sourceNNidx)):
         if targetNNidx[sourceNNidx[i]] == i:
             result.append([i, sourceNNidx[i]])
@@ -49,6 +47,16 @@ def calculate_M_gnd(correspondence, source, target, tao1):
         if distance(source[pair[0]], target[pair[1]]) <= tao1:
             result.append([pair[0], pair[1]])
     return np.array(result)
+
+
+def cal_alignment(source_pc, target_pc, distance=0.05):
+    source_pts = np.asarray(source_pc.points)
+    target_pts = np.asarray(target_pc.points)
+    kdtree_s = KDTree(target_pts)
+
+    sourceNNdis, sourceNNidx = kdtree_s.query(source_pts, 1)
+    ratio = float(np.sum(sourceNNdis < distance) / len(source_pts))
+    return ratio
 
 
 def ransac_based_on_feature_matching(source_keypts, target_keypts, source_desc, target_desc):
@@ -78,11 +86,6 @@ def ransac_based_on_feature_matching(source_keypts, target_keypts, source_desc, 
         ransac_n=4,
         checkers=[],
         criteria=open3d.registration.RANSACConvergenceCriteria(4000000, 500)
-        # source_feature=source_feature,
-        # target_feature=target_feature,
-        # max_correspondence_distance=max_dis,
-        # ransac_n=ransac_n,
-        # criteria=open3d.registration.RANSACConvergenceCriteria(10000, 500)
     )
     return result
 
@@ -93,38 +96,27 @@ def ransac_based_on_correspondence(source_keypts, target_keypts, source_desc, ta
     target_pc = open3d.PointCloud()
     target_pc.points = open3d.utility.Vector3dVector(target_keypts)
 
-    distance_threshold = 0.05
     voxel_size = 0.05
-
     # source_pc = open3d.geometry.voxel_down_sample(source_pc, voxel_size)
     open3d.geometry.estimate_normals(source_pc, open3d.KDTreeSearchParamHybrid(radius=voxel_size * 2, max_nn=30))
     # target_pc = open3d.geometry.voxel_down_sample(target_pc, voxel_size)
     open3d.geometry.estimate_normals(target_pc, open3d.KDTreeSearchParamHybrid(radius=voxel_size * 2, max_nn=30))
 
-    correspondence = calculate_M(source_desc, target_desc)
-    print("Num of correspondence", len(correspondence))
+    start_time = time.time()
+    corr = calculate_M(source_desc[0:2000], target_desc[0:2000])
+    print(time.time() - start_time)
+    print("Num of correspondence", len(corr))
 
     result = open3d.registration_ransac_based_on_correspondence(
         source=source_pc,
         target=target_pc,
-        corres=open3d.utility.Vector2iVector(correspondence),
-        max_correspondence_distance=distance_threshold,
+        corres=open3d.utility.Vector2iVector(corr),
+        max_correspondence_distance=0.05,
         estimation_method=open3d.registration.TransformationEstimationPointToPoint(False),
-        ransac_n=6,
-        criteria=open3d.registration.RANSACConvergenceCriteria(10000, 500)
+        ransac_n=4,
+        criteria=open3d.registration.RANSACConvergenceCriteria(4000000, 500)
     )
-    print(result)
-    print(result.correspondence_set)
-    return result.transformation
-
-
-def calculate_overlap(pc1, pc2, trans):
-    pc1.transform(trans)
-    kdtree = KDTree(pc2)
-    dis = kdtree.query(pc1, 1)
-    count = np.sum(dis > 0.05)
-
-    pass
+    return result
 
 
 def icp_refine(source_pc, target_pc, trans, dis):
@@ -141,32 +133,22 @@ def icp_refine(source_pc, target_pc, trans, dis):
     return result
 
 
-def cal_alignment(source_pc, target_pc, distance=0.05):
-    source_pts = np.asarray(source_pc.points)
-    target_pts = np.asarray(target_pc.points)
-    kdtree_s = KDTree(target_pts)
-
-    sourceNNdis, sourceNNidx = kdtree_s.query(source_pts, 1)
-    ratio = float(np.sum(sourceNNdis < distance) / len(source_pts))
-    return ratio
-
-
 def register2Fragments(id1, id2, pcdpath, keyptspath, ppfdescpath):
     cloud_bin_s = f'cloud_bin_{id1}'
     cloud_bin_t = f'cloud_bin_{id2}'
     # 1. load point cloud, keypoints, descriptors
     original_source_pc = get_pcd(pcdpath, cloud_bin_s)
     original_target_pc = get_pcd(pcdpath, cloud_bin_s)
-    print(original_source_pc)
-    print(original_target_pc)
+    print("original source:", original_source_pc)
+    print("original target:", original_target_pc)
     # downsample and estimate the normals
     voxel_size = 0.02
     source_pc = open3d.geometry.voxel_down_sample(original_source_pc, voxel_size)
     open3d.geometry.estimate_normals(source_pc, open3d.KDTreeSearchParamHybrid(radius=voxel_size * 2, max_nn=30))
     target_pc = open3d.geometry.voxel_down_sample(original_target_pc, voxel_size)
     open3d.geometry.estimate_normals(target_pc, open3d.KDTreeSearchParamHybrid(radius=voxel_size * 2, max_nn=30))
-    print(source_pc)
-    print(target_pc)
+    print("downsampled source:", source_pc)
+    print("downsampled target:", target_pc)
     # load keypoints and descriptors
     source_keypts = get_keypts(keyptspath, cloud_bin_s)
     target_keypts = get_keypts(keyptspath, cloud_bin_t)
@@ -175,7 +157,8 @@ def register2Fragments(id1, id2, pcdpath, keyptspath, ppfdescpath):
     target_desc = get_desc(ppfdescpath, cloud_bin_t)
 
     # 2. ransac
-    ransac_result = ransac_based_on_feature_matching(source_keypts, target_keypts, source_desc, target_desc)
+    ransac_result = ransac_based_on_correspondence(source_keypts, target_keypts, source_desc, target_desc)
+    # ransac_result = ransac_based_on_feature_matching(source_keypts, target_keypts, source_desc, target_desc)
     print("RANSAC Correspondence_set:", len(ransac_result.correspondence_set))
 
     # 3. refine with ICP
@@ -186,7 +169,9 @@ def register2Fragments(id1, id2, pcdpath, keyptspath, ppfdescpath):
     target_pc.transform(icp_result.transformation)
 
     # 5. compute the alignment
+    start_time = time.time()
     align = cal_alignment(original_source_pc, original_target_pc, 0.05)
+    print(time.time() - start_time)
 
     print("align:", align)
     print("trans:", icp_result.transformation)
