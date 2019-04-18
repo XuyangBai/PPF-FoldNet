8b1555636c6b90dc2366e1a127b2d3ddef5bface
xuyangbai <653823597@qq.com>
2019-04-19 00:17:39 +0800

    update dataset, support whole 3dmatch dataset

--- dataset.py
+++ dataset.py
@@ -24,12 +24,21 @@ class SunDataset(data.Dataset):
         self.num_points_per_patch = num_points_per_patch
         self.on_the_fly = on_the_fly
 
-        self.ids_list = [filename.split(".")[0] for filename in os.listdir(self.root)]
-        self.ids_list = sorted(list(set(self.ids_list)))
-        if split == 'train':
-            self.ids_list = self.ids_list[0: int(0.8 * len(self.ids_list))]
-        else:
-            self.ids_list = self.ids_list[int(0.8 * len(self.ids_list)):-1]
+        # Support the whole 3Dmatch dataset
+        with open(os.path.join(root, f'scene_list_{split}.txt')) as f:
+            scene_list = f.readlines()
+
+        self.ids_list = []
+        for scene in scene_list:
+            ids = [scene + "/seq-01/" + str(filename.split(".")[0]) for filename in os.listdir(os.path.join(self.root, scene + '/seq-01/'))]
+            self.ids_list += sorted(list(set(ids)))
+
+        # self.ids_list = [filename.split(".")[0] for filename in os.listdir(self.root)]
+        # self.ids_list = sorted(list(set(self.ids_list)))
+        # if split == 'train':
+        #     self.ids_list = self.ids_list[0: int(0.8 * len(self.ids_list))]
+        # else:
+        #     self.ids_list = self.ids_list[int(0.8 * len(self.ids_list)):-1]
 
     def __getitem__(self, index):
         id = self.ids_list[index]
@@ -53,18 +62,25 @@ class SunDataset(data.Dataset):
 
 
 if __name__ == '__main__':
-    datapath = "./data/train/sun3d-harvard_c11-hv_c11_2/seq-01-train"
+    # datapath = "./data/train/sun3d-harvard_c11-hv_c11_2/seq-01-train"
+    # d = SunDataset(root=datapath, split='train', on_the_fly=True)
+    # print(d.ids_list)
+    # start_time = time.time()
+    # for i in range(10):
+    #     patches, id = d[0]
+    # print(f"On the fly: {time.time() - start_time}")
+    #
+    # datapath = "./data/train/sun3d-harvard_c11-hv_c11_2/seq-01-train-processed"
+    # d = SunDataset(root=datapath, split='train', on_the_fly=False)
+    # print(d.ids_list)
+    # start_time = time.time()
+    # for i in range(10):
+    #     patches, id = d[0]
+    # print(f"Not On the fly: {time.time() - start_time}")
+
+    datapath = "./data/train/"
     d = SunDataset(root=datapath, split='train', on_the_fly=True)
     print(d.ids_list)
-    start_time = time.time()
-    for i in range(10):
-        patches, id = d[0]
-    print(f"On the fly: {time.time() - start_time}")
-
-    datapath = "./data/train/sun3d-harvard_c11-hv_c11_2/seq-01-train-processed"
-    d = SunDataset(root=datapath, split='train', on_the_fly=False)
-    print(d.ids_list)
-    start_time = time.time()
-    for i in range(10):
-        patches, id = d[0]
-    print(f"Not On the fly: {time.time() - start_time}")
+    patches, id = d[0]
+    print(patches.shape)
+    print(id)
